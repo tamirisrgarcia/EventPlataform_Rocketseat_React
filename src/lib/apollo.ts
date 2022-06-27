358afb598e73cbb917f8014d952e032c54bff455
Tamiris Rezende Garcia Costa <tamirisrgarcia@gmail.com>
2022-06-27 12:52:43 -0300

    aula 5

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,20 +1,11 @@
-import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 
-//Configuração extra para tentar ajustar erro de cache e carregamento de vídeo - sem sucesso
-const defaultOptions: DefaultOptions = {
-    watchQuery: {
-        fetchPolicy: 'no-cache',
-        errorPolicy: 'ignore'
-    },
-    query: {
-        fetchPolicy: 'no-cache',
-        errorPolicy: 'all'
-    }
-}
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nlpurp0c1u01xp3izp58ky/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+    },
     cache: new InMemoryCache(),
-    defaultOptions: defaultOptions,
 })
