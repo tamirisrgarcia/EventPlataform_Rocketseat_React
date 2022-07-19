69073707a613686dce6ba65c15a0efa6f43589c6
Tamiris Rezende Garcia Costa <tamirisrgarcia@gmail.com>
2022-07-19 01:17:03 -0300

    adição footer e criação do readme

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+    return(
+        <div className="h-[81px] bg-black w-full flex justify-between items-center text-gray-400 px-8">
+            <div className="flex items-center gap-6"> 
+                <p>Rocketseat - Todos os direitos reservados</p>
+            </div>
+
+            <p>Políticas de privacidade</p>
+        </div>
+    )
+}
